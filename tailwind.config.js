8b511570862d9a809f34d0e24e03ff4b8d44d8f3
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-02-19 00:25:41 -0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        "100%": "100%",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      inset: {
+        "-2": "-2px",
+        2: "2px",
+      },
+      spacing: {
+        "4px": "4px",
+        "24px": "24px",
+        "440px": "440px",
+        "577px": "577px",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
