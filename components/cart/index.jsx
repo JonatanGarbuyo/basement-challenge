708dbd9e5d285894c0fbdc8ae7420ff7c77cfa75
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-02-20 16:57:10 -0300

    add close cart on  escape key pressed

--- components/cart/index.jsx
+++ components/cart/index.jsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from "react";
+import {useCallback, useContext, useEffect, useState} from "react";
 
 import {CartContext} from "../../context/CartContext";
 
@@ -16,20 +16,40 @@ export default function Cart() {
     setCartTotal(total.toFixed(2));
   }, [itemList]);
 
+  const escFunction = useCallback(
+    (event) => {
+      if (event.keyCode === 27) {
+        setIsHidden(true);
+      }
+    },
+    [setIsHidden],
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction);
+    };
+  }, [escFunction]);
+
   return (
     <>
-      <div className={`absolute h-full w-full bg-black opacity-70 z-40 ${isHidden && "hidden"}`} />
+      <div
+        className={`absolute h-full w-full bg-black opacity-70 z-40 ${isHidden && "hidden"}`}
+        onClick={() => setIsHidden(true)}
+      />
       <div
         className={`absolute md:right-0 flex-grow flex flex-col justify-center
           bg-black z-50 border-white md:border-l-1 md:border-b-1 
           h-full w-full md:w-1/3 md:min-w-min  md:h-auto text-center ${isHidden && "hidden"}`}
       >
-        <button
+        <div
           className=" font-bold text-xl text-right py-4 px-2 md:p-6"
           onClick={() => setIsHidden(true)}
         >
-          &rarr; CLOSE
-        </button>
+          <button>&rarr; CLOSE</button>
+        </div>
         <div className="flex flex-col md:flex-row text-9xl md:text-8xl ">
           <h1 className="md:pl-6 font-bold ">YOUR</h1>
           <h1
