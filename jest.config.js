060439a57b617533f3203f40b21f1a02b1a41214
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-03-22 14:50:39 -0300

    jest config modulemapper

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   verbose: true,
   testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "^.+\\.svg$": "ts-jest",
-  },
   setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
+    "^.+\\.svg$": "ts-jest",
     "^@components(.*)$": "<rootDir>/components$1",
     "^@pages(.*)$": "<rootDir>/pages$1",
     "^@hooks(.*)$": "<rootDir>/hooks$1",
